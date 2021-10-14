6649d5e643d46e78d15b27d23e34d4b3a9cdc9df
Matheus <matheuseufrasio2@gmail.com>
2021-10-13 21:29:33 -0300

    utilizando async storage na dashboard | ajuste nos cards de entrada, saída e total

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import { useFocusEffect } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 import { HighlightCard } from '../../components/HighlightCard'
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard'
@@ -18,111 +26,190 @@ import {
   Title,
   TransactionsList,
   LogoutButton,
+  LoadContainer
 } from './styles'
 
 export interface DataListProps extends TransactionCardProps {
   id: string;
 }
 
+interface HighLightProps {
+  amount: string;
+  lastTransaction: string;
+}
+
+interface HighLightData {
+  entries: HighLightProps;
+  outgoings: HighLightProps;
+  total: HighLightProps;
+}
+
+function getLastTransactionDate(
+  collection: DataListProps[],
+  type: 'up' | 'down'
+) {
+
+  const lastTransaction =
+    new Date(
+      Math.max.apply(Math, collection
+        .filter(transaction => transaction.type === type)
+        .map(transaction => new Date(transaction.date).getTime())));
+
+  return `${lastTransaction.getDate()} de ${lastTransaction.toLocaleString('pt-BR', { month: 'long' })}`
+}
+
 export function Dashboard() {
-  const data: DataListProps[] = [
-    {
-      id: '1',
-      type: 'positive',
-      title: "Salário",
-      amount: "R$ 12.000,00",
-      category: {
-        name: 'Vendas',
-        icon: 'dollar-sign'
+  const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [highLightData, setHighLightData] = useState<HighLightData>({} as HighLightData);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const theme = useTheme();
+
+  async function loadTransactions() {
+    const dataKey = "@gofinances:transactions";
+    const response = await AsyncStorage.getItem(dataKey);
+    const transactions = response ? JSON.parse(response) : [];
+
+    let entriesTotal = 0;
+    let outgoingsTotal = 0;
+
+    const transactionsFormatted: DataListProps[] = transactions
+      .map((transaction: DataListProps) => {
+
+        if (transaction.type === 'up') {
+          entriesTotal += Number(transaction.amount);
+        } else {
+          outgoingsTotal += Number(transaction.amount);
+        }
+
+        const amount = Number(transaction.amount)
+          .toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).replace('R$', 'R$ ');
+
+        const date = Intl.DateTimeFormat('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit'
+        }).format(new Date(transaction.date));
+
+        return {
+          id: transaction.id,
+          name: transaction.name,
+          amount,
+          type: transaction.type,
+          category: transaction.category,
+          date,
+        }
+      });
+
+    setTransactions(transactionsFormatted);
+
+    const lastTransactionEntries = getLastTransactionDate(transactions, 'up');
+    const lastTransactionOutgoings = getLastTransactionDate(transactions, 'down');
+    const totalInterval = `01 a ${lastTransactionOutgoings}`;
+
+    const total = entriesTotal - outgoingsTotal;
+
+    setHighLightData({
+      entries: {
+        amount: entriesTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionEntries}`
       },
-      date: "13/04/2020"
-    },
-    {
-      id: '2',
-      type: 'negative',
-      title: "Alimentação",
-      amount: "R$ 1.000,00",
-      category: {
-        name: 'Vendas',
-        icon: 'coffee'
+      outgoings: {
+        amount: outgoingsTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionOutgoings}`
       },
-      date: "13/04/2020"
-    },
-    {
-      id: '3',
-      type: 'negative',
-      title: "Aluguel",
-      amount: "R$ 1.200,00",
-      category: {
-        name: 'Vendas',
-        icon: 'shopping-bag'
-      },
-      date: "13/04/2020"
-    },
-    {
-      id: '4',
-      type: 'positive',
-      title: "Desenvolvimento de site",
-      amount: "R$ 12.000,00",
-      category: {
-        name: 'Vendas',
-        icon: 'dollar-sign'
-      },
-      date: "13/04/2020"
-    }
-  ]
+      total: {
+        amount: total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }),
+        lastTransaction: totalInterval
+      }
+    })
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadTransactions();
+  }, []);
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions();
+  }, []));
 
   return (
     <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/49957773?v=4'}} />
-            <User>
-              <UserGretting>Olá,</UserGretting>
-              <UserName>Matheus</UserName>
-            </User>
-          </UserInfo>
-          <LogoutButton
-            onPress={() => {}}
-          >
-            <Icon name="power" />
-          </LogoutButton>
-        </UserWrapper>
-      </Header>
-      
-      <HighlightCards>
-        <HighlightCard
-          type="up"
-          title="Entrada"
-          amount="R$17.400,00"
-          lastTransaction="Última entrada dia 13 de abril"
-        />
+      <ActivityIndicator />
+      {
+        isLoading ?
+          <LoadContainer>
+            <ActivityIndicator
+              color={theme.colors.primary}
+              size="large"
+            />
+          </LoadContainer> :
+          <>
+            <Header>
+              <UserWrapper>
+                <UserInfo>
+                  <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/49957773?v=4' }} />
+                  <User>
+                    <UserGretting>Olá,</UserGretting>
+                    <UserName>Matheus</UserName>
+                  </User>
+                </UserInfo>
+                <LogoutButton
+                  onPress={() => { }}
+                >
+                  <Icon name="power" />
+                </LogoutButton>
+              </UserWrapper>
+            </Header>
 
-        <HighlightCard 
-          type="down"
-          title="Saídas"
-          amount="R$1.244,56"
-          lastTransaction="Última entrada dia 03 de abril"
-        />
+            <HighlightCards>
+              <HighlightCard
+                type="up"
+                title="Entrada"
+                amount={highLightData.entries.amount}
+                lastTransaction={highLightData.entries.lastTransaction}
+              />
 
-        <HighlightCard
-          type="total"
-          title="Total"
-          amount="R$16.120,00"
-          lastTransaction="01 à 13 de abril"
-        />
-      </HighlightCards>
-    
-      <Transactions>
-        <Title>Listagens</Title>
+              <HighlightCard
+                type="down"
+                title="Saídas"
+                amount={highLightData.outgoings.amount}
+                lastTransaction={highLightData.outgoings.lastTransaction}
+              />
 
-        <TransactionsList
-          data={data}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => <TransactionCard data={item} />}
-        />
-      </Transactions>
+              <HighlightCard
+                type="total"
+                title="Total"
+                amount={highLightData.total.amount}
+                lastTransaction={highLightData.total.lastTransaction}
+              />
+            </HighlightCards>
+
+            <Transactions>
+              <Title>Listagens</Title>
+
+              <TransactionsList
+                data={transactions}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <TransactionCard data={item} />}
+              />
+            </Transactions>
+          </>
+      }
+
     </Container>
   )
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -9,7 +9,12 @@ import {
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
 import { useForm } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
+
+import uuid from 'react-native-uuid';
 
 import { Button } from '../../components/Form/Button';
 import { CategorySelectButton } from '../../components/Form/CategorySelectButton';
@@ -46,11 +51,15 @@ const schema = Yup.object().shape({
 export function Register() {
   const {
     control,
+    reset,
     handleSubmit,
     formState: { errors }
   } = useForm({
     resolver: yupResolver(schema)
   });
+
+  const navigation = useNavigation();
+
 
   const [transactionType, setTransactionType] = useState('');
   const [categoryModalOpen, setCategoryModalOpen] = useState(false);
@@ -71,19 +80,45 @@ export function Register() {
   function handleOpenSelectcategoryModal() {
     setCategoryModalOpen(true);
   }
-  function handleRegister(form: FormData) {
+
+  async function handleRegister(form: FormData) {
     if (!transactionType) return Alert.alert('Selecione o tipo da transação.'); 
     
     if (category.key === 'category') return Alert.alert('Selecione a categoria.'); 
     
-    const data = {
+    const newTransaction = {
+      id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
-      transactionType,
-      category: category.key
+      type: transactionType,
+      category: category.key,
+      date: new Date()
     }
 
-    console.log(data)
+    try {
+      const dataKey = "@gofinances:transactions";
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ];
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted)); 
+
+      reset();
+      setTransactionType('');
+      setCategory({
+        key: 'category',
+        name: 'Categoria'
+      });
+
+      navigation.navigate("Listagem");
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Não foi possível salvar.")
+    }
   }
 
   return (
